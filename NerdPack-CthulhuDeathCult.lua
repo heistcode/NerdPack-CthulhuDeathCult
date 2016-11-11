5556fd041fd121f7b370872b3322756d7f64d017
heist <heist.code@me.com>
2016-11-11 00:11:14 -0500

    Remove NerdPack-Xeer Dependency

--- NerdPack-CthulhuDeathCult.lua
+++ NerdPack-CthulhuDeathCult.lua
@@ -12,6 +12,7 @@ local UnitHealthMax        = UnitHealthMax
 local SecureCmdOptionParse = SecureCmdOptionParse
 local RunMacroText         = RunMacroText
 local select               = select
+local floor                = floor
 local pairs                = pairs
 local NeP                  = NeP
 
@@ -81,12 +82,17 @@ function CDC.VehicleUICallback(button, down)
 	end
 end
 
-NeP.DSL:Register("boss_time_to_die", function(target)
+NeP.DSL:Register("bossttd", function(target)
 	return NeP.DSL:Get("boss")(target) and NeP.DSL:Get("deathin")(target) or 8675309
 end)
 
 NeP.DSL:Register("relativehealth", function(target)
 	return UnitHealth(target) / UnitHealthMax("player")
+end)
+
+NeP.DSL:Register('shaste', function()
+	local shaste = NeP.DSL:Get('haste')('player')
+	return floor((100 / ( 100 + shaste )) * 10^3 ) / 10^3
 end)
 
 CDC.GUI = {
@@ -119,7 +125,7 @@ function CDC.ExeOnLoad()
 	NeP.CustomKeybind:Add(CDC.Name, "2", CDC.VehicleUICallback)
 
 	NeP.Interface:AddToggle({
-		key  = "xCombustion",
+		key  = "combustion",
 		name = "Combustion",
 		text = "ON/OFF using Combustion in rotation",
 		icon = "Interface\\Icons\\Spell_fire_sealoffire",
@@ -131,12 +137,12 @@ function CDC.ExeOnUnload()
 end
 
 local cancast =
-	"{!moving || player.buff(Ice Floes) || lastgcd(Ice Floes) || cooldown(Ice Floes).remains = 0}"
+	"{!moving || movingfor > 0.1 || player.buff(Ice Floes) || lastgcd(Ice Floes) || spell(Ice Floes).cooldown = 0}"
 
 local CastFireball = {
 	{
 		"Ice Floes",
-		"cooldown(61304).remains < 0.1 & moving & !lastgcd(Ice Floes) & !player.buff(Ice Floes)"
+		"spell(61304).cooldown < 0.1 & moving & movingfor > 0.1 & !lastgcd(Ice Floes) & !player.buff(Ice Floes)"
 	},
 	{"Fireball"}
 }
@@ -144,7 +150,7 @@ local CastFireball = {
 local CastPyroblast = {
 	{
 		"Ice Floes",
-		"cooldown(61304).remains < 0.1 & moving & !lastgcd(Ice Floes) & !player.buff(Ice Floes)"
+		"spell(61304).cooldown < 0.1 & moving & movingfor > 0.1 & !lastgcd(Ice Floes) & !player.buff(Ice Floes)"
 	},
 	{"Pyroblast"}
 }
@@ -171,10 +177,10 @@ local Survival = {
 	{
 		"!Ice Block",
 		"{customkeybind(shift q) || {player.combat & player.health < 15 & !player.buff(Ice Barrier) &" ..
-				"spell.cooldown(Ice Barrier) > 0 & !player.buff(Combustion) & !player.buff(Rune of Power)}}" ..
+				"spell(Ice Barrier).cooldown > 0 & !player.buff(Combustion) & !player.buff(Rune of Power)}}" ..
 				"& !player.buff(Ice Block) & !player.debuff(Hypothermia)"
 	},
-	{"#127834", "player.health < 20 & !player.buff(Ice Barrier) & spell.cooldown(Ice Barrier) > 0"},
+	{"#127834", "player.health < 20 & !player.buff(Ice Barrier) & spell(Ice Barrier).cooldown > 0"},
 	{
 		"Ice Barrier",
 		"{customkeybind(q) || player.health < 30} & !player.buff(Ice Barrier) &" ..
@@ -186,42 +192,42 @@ local Survival = {
 local Talents = {
 	{
 		"Flame On",
-		"talent(4,2) & {action(Fire Blast).charges < 0.2 & {cooldown(Combustion).remains > 65 ||" ..
-		"target.boss_time_to_die < cooldown(Combustion).remains}}"
+		"talent(4,2) & {spell(Fire Blast).charges < 0.2 & {spell(Combustion).cooldown > 65 ||" ..
+		"target.bossttd < spell(Combustion).cooldown}}"
 	},
 	{
 		"Blast Wave",
 		"talent(4,1) & {!player.buff(Combustion) || {player.buff(Combustion) &" ..
-		"action(Fire Blast).charges < 1 & action(Phoenix's Flames).charges < 1}}"
+		"spell(Fire Blast).charges < 1 & spell(Phoenix's Flames).charges < 1}}"
 	},
 	{
 		"Meteor",
-		"talent(7,3) & {cooldown(Combustion).remains > 30 || {cooldown(Combustion).remains >" ..
-		"target.boss_time_to_die} || player.buff(Rune of Power)}"
+		"talent(7,3) & {spell(Combustion).cooldown > 30 || {spell(Combustion).cooldown >" ..
+		"target.bossttd} || player.buff(Rune of Power)}"
 	},
 	{
 		"Cinderstorm",
-		"talent(7,2) & {cooldown(Combustion).remains < action(Cinderstorm).cast_time &" ..
-		"{player.buff(Rune of Power) || !talent(3,2)} || cooldown(Combustion).remains > 10 * spell_haste" ..
+		"talent(7,2) & {spell(Combustion).cooldown < spell(Cinderstorm).casttime &" ..
+		"{player.buff(Rune of Power) || !talent(3,2)} || spell(Combustion).cooldown > 10 * shaste" ..
 		"& !player.buff(Combustion)}"
 	},
 	{"Dragon's Breath", "equipped(132863)"},
 	{
 		"Living Bomb",
-		"talent(6,1) & {player.area(40).enemies > 1 || customkeybind(2)} & !player.buff(Combustion)"
+		"talent(6,1) & {player.area(40).enemies > 1 || customkeybind(1)} & !player.buff(Combustion)"
 	}
 }
 
 local CombustionRotation = {
+	{Talents},
 	{"#139326", "equipped(139326)"},
 	{"#127843", "UI(potion) & hashero & boss1.exists"},
 	{
 		"#132510",
 		"UI(gunpowder) & {target.relativehealth > 5 || target.boss} & !{UI(potion) & boss1.exists} &" ..
-		"player.buff(Combustion) & player.buff(Rune of Power) & player.buff(Pyretic Incantation).stack = 5"
+		"player.buff(Combustion) & player.buff(Rune of Power) & player.buff(Pyretic Incantation).count = 5"
 	},
 	{"Rune of Power", "!player.buff(Combustion)"},
-	{Talents},
 	{
 		"&Combustion",
 		"player.buff(Rune of Power) || {player.casting(Rune of Power) & player.casting.percent > 80}"
@@ -231,19 +237,19 @@ local CombustionRotation = {
 	{"&Pyroblast", "player.buff(Hot Streak!) & player.buff(Combustion)"},
 	{
 		"Phoenix's Flames",
-		"action(Phoenix's Flames).charges>2.7 & player.buff(Combustion) & !player.buff(Hot Streak!)"
+		"spell(Phoenix's Flames).charges>2.7 & player.buff(Combustion) & !player.buff(Hot Streak!)"
 	},
 	{"&Fire Blast", "player.buff(Heating Up) & !player.lastcast(Fire Blast) & player.buff(Combustion)"},
-	{"Phoenix's Flames", "artifact(Phoenix's Flames).equipped"},
-	{"Scorch", "player.buff(Combustion).remains > action(Scorch).cast_time"},
+	{"Phoenix's Flames"},
+	{"Scorch", "player.buff(Combustion).duration > spell(Scorch).casttime"},
 }
 
 local MainRotation = {
 	{
 		"Pyroblast",
-		"player.buff(Hot Streak!) & player.buff(Hot Streak!).remains < action(Fireball).execute_time"
+		"player.buff(Hot Streak!) & player.buff(Hot Streak!).duration < spell(Fireball).casttime"
 	},
-	{"Phoenix's Flames", "action(Phoenix's Flames).charges > 2.7"},
+	{"Phoenix's Flames", "spell(Phoenix's Flames).charges > 2.7"},
 	{
 		"Flamestrike",
 		"talent(6,3) & target.area(10).enemies > 2 & player.buff(Hot Streak!)",
@@ -254,43 +260,43 @@ local MainRotation = {
 		"player.buff(Hot Streak!) & {lastgcd(Fireball) || player.casting(Fireball) ||" ..
 				"player.casting(Pyroblast) || !" .. cancast .. "}"
 	},
+	{Talents},
 	{"Pyroblast", "player.buff(Hot Streak!) & target.health <= 25 & equipped(132454)"},
 	{
 		CastPyroblast,
 		"target.relativehealth > 1 & !player.casting(Pyroblast) & !player.buff(Hot Streak!) &" ..
-				"player.buff(Kael'thas's Ultimate Ability).remains > action(Pyroblast).execute_time + gcd" ..
+				"player.buff(Kael'thas's Ultimate Ability).duration > spell(Pyroblast).casttime + gcd" ..
 				"&" .. cancast
 	},
-	{"Phoenix's Flames", "!player.buff(Hot Streak!) & action(Phoenix's Flames).charges > 2.7"},
-	{Talents},
+	{"Phoenix's Flames", "!player.buff(Hot Streak!) & spell(Phoenix's Flames).charges > 2.7"},
 	{
 		"&Fire Blast",
 		"!talent(7,1) & player.buff(Heating Up) & {player.casting(Fireball) ||" ..
 				"player.casting(Pyroblast) || !".. cancast .."} & !lastcast(Fire Blast) &" ..
-				"{!talent(3,2) || action(Fire Blast).charges > 1.4 || cooldown(Combustion).remains < 40} &" ..
-				"{3 - action(Fire Blast).charges} * {12 * spell_haste} <= cooldown(Combustion).remains + 3" ..
-				"|| target.boss_time_to_die < 4"
+				"{!talent(3,2) || spell(Fire Blast).charges > 1.4 || spell(Combustion).cooldown < 40} &" ..
+				"{3 - spell(Fire Blast).charges} * {12 * shaste} <= spell(Combustion).cooldown + 3" ..
+				"|| target.bossttd < 4"
 	},
 	{
 		"&Fire Blast",
 		"talent(7,1) & player.buff(Heating Up) & {player.casting(Fireball) ||" ..
 				"player.casting(Pyroblast) || !" ..	cancast .. "} & !lastcast(Fire Blast) &" ..
-				"{!talent(3,2) || action(Fire Blast).charges > 1.5 || {cooldown(Combustion).remains < 40}}" ..
-				"& {3 - action(Fire Blast).charges} * {18 * spell_haste} <= cooldown(Combustion).remains +" ..
-				"3 || target.boss_time_to_die < 4"
+				"{!talent(3,2) || spell(Fire Blast).charges > 1.5 || {spell(Combustion).cooldown < 40}}" ..
+				"& {3 - spell(Fire Blast).charges} * {18 * shaste} <= spell(Combustion).cooldown +" ..
+				"3 || target.bossttd < 4"
 	},
 	{
 		"Phoenix's Flames",
 		"!player.buff(Hot Streak!) & {player.buff(Combustion) || {player.buff(Rune of Power) &" ..
-				"player.buff(Pyretic Incantation).stack > 3} || player.buff(Incanter's Flow).stack > 3 ||" ..
-				"talent(3,1)} & {4 - action(Phoenix's Flames).charges} * 13 < cooldown(Combustion).remains" ..
-				"+ 5 || target.boss_time_to_die < 10"
+				"player.buff(Pyretic Incantation).count > 3} || player.buff(Incanter's Flow).count > 3 ||" ..
+				"talent(3,1)} & {4 - spell(Phoenix's Flames).charges} * 13 < spell(Combustion).cooldown" ..
+				"+ 5 || target.bossttd < 10"
 	},
 	{
 		"Phoenix's Flames",
 		"!player.buff(Hot Streak!) & {player.buff(Combustion) || {player.buff(Rune of Power) &" ..
-				"player.buff(Pyretic Incantation).stack > 3}} & {4 - action(Phoenix's Flames).charges} * 30" ..
-				"< cooldown(Combustion).remains + 5"
+				"player.buff(Pyretic Incantation).count > 3}} & {4 - spell(Phoenix's Flames).charges} * 30" ..
+				"< spell(Combustion).cooldown + 5"
 	},
 	{"Scorch", "target.health <= 25 & equipped(132454)"},
 	{CastFireball, cancast},
@@ -305,16 +311,16 @@ local Combat = {
 	{
 		"Rune of Power",
 		"{target.relativehealth > 1 || target.boss} & toggle(cooldowns) & !moving &" ..
-				"{cooldown(Combustion).remains > 40 || !toggle(xCombustion)} & {!player.buff(Combustion) &" ..
-				"{cooldown(Flame On).remains < 5 || cooldown(Flame On).remains > 30} & !talent(7,1) ||" ..
-				"target.boss_time_to_die < 11 || talent(7,1) & {action(Rune of Power).charges > 1.8 ||" ..
-				"player.combat.time < 40} & {cooldown(Combustion).remains > 40 || !toggle(xCombustion)}}"
+				"{spell(Combustion).cooldown > 40 || !toggle(combustion)} & {!player.buff(Combustion) &" ..
+				"{spell(Flame On).cooldown < 5 || spell(Flame On).cooldown > 30} & !talent(7,1) ||" ..
+				"target.bossttd < 11 || talent(7,1) & {spell(Rune of Power).charges > 1.8 ||" ..
+				"player.combat.time < 40} & {spell(Combustion).cooldown > 40 || !toggle(combustion)}}"
 	},
 	{
 		CombustionRotation,
 		"player.buff(Combustion) || {{target.relativehealth > 1 || target.boss} &" ..
-				"toggle(xCombustion) & toggle(cooldowns) & {!moving || player.buff(Combustion)} &" ..
-				"{cooldown(Combustion).remains <= action(Rune of Power).cast_time || player.buff(Combustion)}}"
+				"toggle(combustion) & toggle(cooldowns) & {!moving || player.buff(Combustion)} &" ..
+				"{spell(Combustion).cooldown <= spell(Rune of Power).casttime || player.buff(Combustion)}}"
 	},
 	{MainRotation}
 }
@@ -330,7 +336,10 @@ local IC = {
 local OOC = {
 	{Keybinds},
 	{Survival},
-	{"&/stopcasting", "!customkeybind(1) & !customkeybind(2) & player.casting(Fireball)"},
+	{
+		"&/stopcasting",
+		"!customkeybind(1) & !customkeybind(2) & player.casting(Fireball) & player.casting.percent < 80"
+	},
 	{CastFireball, "customkeybind(1) &".. cancast},
 	{IC, "customkeybind(2)"},
 }
